bc551e2e154386ba08de6fb96c17f4a816f1e470
hananfarizta <hananfariztaa@gmail.com>
2024-07-05 10:07:31 +0700

    Initial Commit

---
+++ Animated/AnimatedApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedApp.swift
+//  Animated
+//
+//  Created by Hanan Farizta on 05/07/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
